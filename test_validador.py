1b5fb0793d01115753ceb41c72a65282280ef382
Andres Goonzalez Licea <2025310434@uteq.edu.mx>
2026-07-27 20:06:41 -0600

    Update test_validador.py

--- test_validador.py
+++ test_validador.py
@@ -2,20 +2,18 @@ import pytest
 from validador import es_password_valida
 
 def test_password_corta_devuelve_false():
-    # Arrange
     password_corta = "abc"
-
-    # Act
     resultado = es_password_valida(password_corta)
-
-    # Assert
     assert resultado is False
 
-def es_password_valida(password: str) -> bool:
-    return len(password) >= 8
 
 def test_password_sin_numero_devuelve_false():
-    # Contraseña con 8+ caracteres pero sin números
+    password_sin_num = "abcdefgh"
+    resultado = es_password_valida(password_sin_num)
+    assert resultado is False
+
+def test_password_sin_numero_devuelve_false():
+
     password_sin_num = "abcdefgh"
 
     resultado = es_password_valida(password_sin_num)
