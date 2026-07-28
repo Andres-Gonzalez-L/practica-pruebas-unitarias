d4029432db46561565c8268f15e34d1697d634eb
Andres Goonzalez Licea <2025310434@uteq.edu.mx>
2026-07-27 19:45:22 -0600

    Update validador.py

---
+++ validador.py
@@ -0,0 +1,8 @@
+def es_password_valida(password: str) -> bool:
+
+    tiene_longitud = len(password) >= 8
+
+    tiene_numero = any(caracter.isdigit() for caracter in password)
+
+
+    return tiene_longitud and tiene_numero
