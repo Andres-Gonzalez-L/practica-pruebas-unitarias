29cdeda0bfc47477f7aa51f01d75ce9daeac89db
coco1016t <gcoco6807@gmail.com>
2026-07-27 19:19:30 -0600

    Update test_validador.py

--- test_validador.py
+++ test_validador.py
@@ -10,3 +10,14 @@ def test_password_corta_devuelve_false():
 
     # Assert
     assert resultado is False
+
+def es_password_valida(password: str) -> bool:
+    return len(password) >= 8
+
+def test_password_sin_numero_devuelve_false():
+    # Contraseña con 8+ caracteres pero sin números
+    password_sin_num = "abcdefgh"
+
+    resultado = es_password_valida(password_sin_num)
+
+    assert resultado is False
